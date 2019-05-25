b8b35b52b928065a8fa71025ed2bca9512e04913
Nabid Farvez <farvezna@gmail.com>
2019-05-25 10:06:54 -0400

    Checking datetime retrieval and write to local csv

--- discord_job_clock.py
+++ discord_job_clock.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
+import datetime
 
 bot = commands.Bot(command_prefix='$')
 
+employeeDict = {}
+
 @bot.event
 async def on_ready():
+    #TODO: add users as keys and allow command to update Name--hide users but add name to spreadsheet
     print("Bot is ready and let's goooo!")
     await bot.change_presence(activity=discord.Game(name="at work but getting paid!"))
 
@@ -13,6 +17,10 @@ async def on_message(message):
     if message.author == bot.user:
         return
     if message.content == 'ping':
-        await message.channel.send('pong')
+        timestamp = datetime.datetime.now()
+        with open("datalog.csv", 'a') as fout:
+            fout.write(timestamp.strftime('%x'))
+        await message.channel.send('pong: **' + timestamp.strftime('%x') + "**")
+        await message.channel.send('**' + timestamp.strftime('%X') + '**')
 
 bot.run(TOKEN)
