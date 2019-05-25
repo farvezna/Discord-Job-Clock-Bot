49a336bb4c8c03d15adb226f97a49f1cf3906d90
Nabid Farvez <farvezna@gmail.com>
2019-05-25 09:24:56 -0400

    Basic setup of bot

--- discord_job_clock.py
+++ discord_job_clock.py
@@ -1,1 +1,18 @@
-#pull/push test to git from local
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print("Bot is ready and let's goooo!")
+    await bot.change_presence(activity=discord.Game(name="at work but getting paid!"))
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content == 'ping':
+        await message.channel.send('pong')
+
+bot.run(TOKEN)
